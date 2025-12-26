a6138a33a20226f263950d0f491ea5fb31174d9c
Hamza Ahmed Shaikh <99156466+hamzaahmedshaikh@users.noreply.github.com>
2025-12-27 04:34:03 +0500

    commit

--- app.js
+++ app.js
@@ -5,8 +5,29 @@ description:"Most asked JavaScript interview questions",
 channel:"Muhammad Hasan Dev",
 views:"317K views",
 time:"1 year ago",
+duration:"1:35:43",
 category:"coding",
 thumb:"https://i.ytimg.com/vi/hdI2bqOjy3c/maxresdefault.jpg"
+},
+{
+title:"Full Stack Developer Roadmap",
+description:"Step by step full stack roadmap",
+channel:"Muhammad Hasan Dev",
+views:"210K views",
+time:"8 months ago",
+duration:"42:11",
+category:"coding",
+thumb:"https://i.ytimg.com/vi/9He4UBLyk8Y/maxresdefault.jpg"
+},
+{
+title:"Advanced JavaScript Concepts",
+description:"Closures, hoisting, async explained",
+channel:"Muhammad Hasan Dev",
+views:"180K views",
+time:"5 months ago",
+duration:"58:22",
+category:"coding",
+thumb:"https://i.ytimg.com/vi/PoRJizFvM7s/maxresdefault.jpg"
 },
 {
 title:"Breaking News Pakistan",
@@ -14,17 +35,49 @@ description:"Latest political updates",
 channel:"Dunya News",
 views:"12K views",
 time:"2 hours ago",
+duration:"8:36",
 category:"news",
 thumb:"https://i.ytimg.com/vi/tgbNymZ7vqY/maxresdefault.jpg"
 },
 {
-title:"Podcast: Reality Talks",
-description:"Deep discussion podcast",
+title:"PIA Privatized Successfully",
+description:"Shocking news for Pakistan",
+channel:"24 News HD",
+views:"678 views",
+time:"1 hour ago",
+duration:"12:24",
+category:"news",
+thumb:"https://i.ytimg.com/vi/ysz5S6PUM-U/maxresdefault.jpg"
+},
+{
+title:"Reality Talk Podcast",
+description:"Deep discussion on society",
 channel:"Hajira Umer",
 views:"98K views",
 time:"3 days ago",
+duration:"1:54:11",
 category:"podcast",
 thumb:"https://i.ytimg.com/vi/PkZNo7MFNFg/maxresdefault.jpg"
+},
+{
+title:"Indian Pop Mix",
+description:"Trending Indian pop songs",
+channel:"YouTube Mix",
+views:"1.2M views",
+time:"1 week ago",
+duration:"2:15:00",
+category:"music",
+thumb:"https://i.ytimg.com/vi/ysz5S6PUM-U/maxresdefault.jpg"
+},
+{
+title:"Coding Motivation",
+description:"Why you should learn programming",
+channel:"Muhammad Hasan Dev",
+views:"54K views",
+time:"2 weeks ago",
+duration:"15:40",
+category:"coding",
+thumb:"https://i.ytimg.com/vi/hdI2bqOjy3c/maxresdefault.jpg"
 }
 ]
 
@@ -38,7 +91,10 @@ list.forEach(v=>{
 const el=document.createElement("div")
 el.className="card"
 el.innerHTML=`
-<img src="${v.thumb}">
+<div class="thumb-wrap">
+<img class="thumb" src="${v.thumb}">
+<span class="duration">${v.duration}</span>
+</div>
 <div class="info">
 <img src="profile.jpg">
 <div class="text">
